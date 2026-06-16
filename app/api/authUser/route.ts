2cf76b656ac373547136d9fae5366774ea06aec5
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-06-17 01:28:51 +0530

    change: authUser

--- app/api/authUser/route.ts
+++ app/api/authUser/route.ts
@@ -2,17 +2,20 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const cookieStore = cookies();
-  const token = (await cookieStore).get("token")?.value;
+  // const cookieStore = cookies();
+  // const token = (await cookieStore).get("token")?.value;
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     Authorization: `Bearer ${token}`,
+  //   },
+  // });
 
-  const result = await res.json();
+  // const result = await res.json();
+  const result = {
+    name: "Rashmika",
+  };
   return NextResponse.json({ success: true, user: result });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
-    return (
-        <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black p-5">
-            {/* landing page goes here */}
-        </main>
-    );
+  return (
+    <main className="w-screen h-screen overflow-hidden">
+      <video
+        src={"./videos/intro.mp4"}
+        className="w-full h-full object-cover"
+        autoPlay
+      />
+      <Link href={"/feed"}>
+        <Button className="cursor-pointer fixed bottom-20 left-1/2 -translate-x-1/2 p-5 animate-pulse">
+          CLICK TO CONTINUE
+        </Button>
+      </Link>
+    </main>
+  );
 }

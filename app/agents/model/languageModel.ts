23075a7d469c4c2ca6b72e5278382c010efdf61b
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-06-24 00:21:24 +0530

    update #5

--- app/agents/model/languageModel.ts
+++ app/agents/model/languageModel.ts
@@ -3,15 +3,15 @@
 import { ChatGroq } from "@langchain/groq";
 
 export const llm = new ChatGroq({
-  // apiKey: "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",
-  apiKey: process.env.GROQ_API_KEY,
+  apiKey: "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",
+  // apiKey: process.env.GROQ_API_KEY,
   // model: "meta-llama/llama-4-scout-17b-16e-instruct",
   model: "llama-3.3-70b-versatile",
   // maxTokens: 500,
   // maxRetries: 2,
 });
 export const imageModel = new ChatGroq({
-  apiKey: process.env.GROQ_API_KEY,
+  apiKey: "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",
 
   model: "meta-llama/llama-4-scout-17b-16e-instruct",
   // model: "llama-3.3-70b-versatile",

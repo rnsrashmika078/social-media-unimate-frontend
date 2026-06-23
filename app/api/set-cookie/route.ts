8d3c092c413bbc17cc94f4aee1b5ef57abfdb919
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-06-24 02:21:27 +0530

    update: cookies route #1

--- app/api/set-cookie/route.ts
+++ app/api/set-cookie/route.ts
@@ -10,10 +10,7 @@ export async function POST(request: NextRequest) {
     sameSite: "lax",
   });
 
-  // return NextResponse.json({
-  //   success: true,
-  // });
-  const baseURL = process.env.NEXT_PUBLIC_API_URL!;
-  return NextResponse.redirect(new URL("http://20.207.201.226:3000/feed"));
-  // non prod request.nextUrl
+  return NextResponse.json({
+    success: true,
+  });
 }

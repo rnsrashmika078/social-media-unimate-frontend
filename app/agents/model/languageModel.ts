651a85227d4f507b9067a13f6c42dc35ddef0c9c
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-06-24 00:15:45 +0530

    update #3

--- app/agents/model/languageModel.ts
+++ app/agents/model/languageModel.ts
@@ -1,11 +1,9 @@
-export const dynamic = 'force-dynamic';
-
 // ... your existing imports (e.g., import { ChatGroq } from "@langchain/groq")
 // ... your existing code
 import { ChatGroq } from "@langchain/groq";
 
 export const llm = new ChatGroq({
-  apiKey: process.env.GROQ_API_KEY!,
+  apiKey: "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",
   // model: "meta-llama/llama-4-scout-17b-16e-instruct",
   model: "llama-3.3-70b-versatile",
   // maxTokens: 500,

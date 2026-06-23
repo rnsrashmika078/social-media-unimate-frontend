74314a6c37b13400018245dfe63da7cace37b6fc
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-06-23 20:52:07 +0530

    fix: page.tsx

--- app/(public)/feed/page.tsx
+++ app/(public)/feed/page.tsx
@@ -1,35 +1,13 @@
 import { getPosts } from "@/app/helper/posts";
 import AddPost from "@/components/main/AddPost";
 import Post from "@/components/main/Post";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-import axios from "axios";
 const FeedPage = async () => {
-  // const queryClient = new QueryClient();
-  // await queryClient.prefetchInfiniteQuery({
-  //   queryKey: ["getPosts"],
-  //   queryFn: ({ pageParam = 1 }) => getPosts({ page: pageParam }),
-  //   initialPageParam: 1,
-  // });
-  // const dehydratedState = dehydrate(queryClient);
-
-  // const posts = getPosts({ page: 1 }, {});
-  // let allPosts = [];
   const posts = await getPosts({ page: 1 });
-  // if (posts.posts) {
-  //   allPosts = posts.posts.value.data;
-  // }
 
-  // console.log("ALL POSTS", posts.data);
   return (
     <>
-      {/* <HydrationBoundary state={dehydratedState}> */}
       <AddPost />
       <Post posts={posts.data} />
-      {/* </HydrationBoundary> */}
     </>
   );
 };
